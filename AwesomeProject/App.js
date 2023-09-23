4dce981bfee50250a1728c3e61dc91c1ccd2a125
Aaliyah Reid <aaliyahreid12345@gmail.com>
2023-09-23 15:00:09 -0400

    g

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,19 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import GetStarted from './page/GetStarted';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
 
 export default function App() {
   return (
+    <NavigationContainer> 
+    <Stack.Navigator>
+    <Stack.Screen name="Home" component={HomeScreen} />
+    <Stack.Screen name="Profile" component={ProfilePage} />
+    <Stack.Screen name="Camera" component={Camera} />
+
+    </Stack.Navigator>
     <View style={styles.container}>
-      <GetStarted></GetStarted>
-
-
-
-      
-
-
+      <Text></Text>
       <StatusBar style="auto" />
-    </View>
+      <Button
+        title="Go to Home"
+        onPress={() => navigation.navigate('HomeScreen')}
+      />
+      Button
+        title="Go to Profile"
+        onPress={() => navigation.navigate('Profile')}
+      />
+
+      Button
+        title="Go to Profile"
+        onPress={() => navigation.navigate('Camera')}
+      />
+
+    </View> 
+    </NavigationContainer>
+    
   );
 }
 
